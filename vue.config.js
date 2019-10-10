c6d1cb985a083c5adac91abd28f1ddcf1b25d2c8
小火车况且况且况且 <1457639270@qq.com>
2019-10-10 14:55:57 +0800

    新建项目

---
+++ vue.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+function resolve (dir) {
+  return path.join(__dirname, dir)
+}
+
+module.exports = {
+  publicPath: './',
+  outputDir: process.env.VUE_APP_TITLE === 'production' ? 'dist' : 'test',
+  assetsDir: '',
+  indexPath: 'index.html',
+  filenameHashing: true,
+  lintOnSave: false,
+  runtimeCompiler: false,
+  transpileDependencies: [],
+  productionSourceMap: false,
+  integrity: false,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': resolve('src')
+      }
+    }
+  },
+  devServer: {
+    port: 8080,
+    host: '0.0.0.0',
+    open: true,
+    https: false,
+    overlay: {
+      warnings: true,
+      errors: true
+    }
+  }
+}
