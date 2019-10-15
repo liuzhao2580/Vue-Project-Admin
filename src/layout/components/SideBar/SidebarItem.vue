5ebfadd17259628a5706547402b5cacbfde0e57e
liuzhao2580 <1457639270@qq.com>
2019-10-15 21:34:49 +0800

    侧边栏完成, 高亮问题待修改

--- src/layout/components/SideBar/SidebarItem.vue
+++ src/layout/components/SideBar/SidebarItem.vue
@@ -4,20 +4,20 @@
         <template v-if="checkMoreRouter(item.children)">
             <!-- 说明只有一级菜单 -->
             <template v-if="!item.meta">
-                 <el-menu-item>
+                 <el-menu-item :index="basePath">
                     <i class="el-icon-menu"></i>
                     <span slot="title">{{item.children[0].meta.title}}</span>
                 </el-menu-item>
             </template>
             <!-- 说明存在二级菜单 -->
-            <template else>
-                <el-submenu>
+            <template v-else>
+                <el-submenu :index="item.path">
                     <template slot="title">
                         <i class="el-icon-location"></i>
-                        <span slot="title">{{item}}</span>
+                        <span slot="title">{{item.meta.title}}</span>
                     </template>
                     <el-menu-item-group v-for="groupItem in item.children" :key="groupItem.path">
-                        <el-menu-item>
+                        <el-menu-item :index="item.path + '/' +groupItem.path">
                             <i class="el-icon-menu"></i>
                             <span slot="title">{{groupItem.meta.title}}</span>
                         </el-menu-item>
@@ -49,9 +49,7 @@ export default {
 		return {};
 	},
 	created() {},
-	mounted() {
-        console.log(this.item)
-    },
+	mounted() {},
 	methods: {
         // 用户判断多级路由
         checkMoreRouter(Citem) {
