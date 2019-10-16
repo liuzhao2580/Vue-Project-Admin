060b5227eb44f52266a193f1d55f530d8f3d0e47
小火车况且况且况且 <1457639270@qq.com>
2019-10-16 14:48:13 +0800

    添加vuex的user模块

---
+++ src/store/modules/user.js
@@ -0,0 +1,15 @@
+const state = {
+
+}
+const mutations = {
+
+}
+const actions = {
+
+}
+export default {
+    namespaced: true, // 之后在不同页面中引入getter、actions、mutations时，需要加上所属的模块名
+    state,
+    mutations,
+    actions
+}
