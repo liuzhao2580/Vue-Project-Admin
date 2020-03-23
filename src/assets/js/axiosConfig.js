40ef7c56c6ad852c32422ad5becef6f02fd8effc
liuzhao2580 <1457639270@qq.com>
2020-03-23 14:05:52 +0800

    '添加axios的请求头设置token'

--- src/assets/js/axiosConfig.js
+++ src/assets/js/axiosConfig.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
+const { getCookie } = require("@/utils/cookies")
 const Axios = axios.create({
     // baseURL: baseURL, // url = base url + request url
     // withCredentials: true, // send cookies when cross-domain requests
     timeout: 5000 // request timeout
 })
+Axios.defaults.headers.common["Authorization"] = getCookie("token")
 export default Axios
