b02799b9fcc66169358ae949f076414b786e61a5
liuzhao2580 <1457639270@qq.com>
2020-10-19 16:10:52 +0800

    使用render，替代template标签，解决侧边栏折叠出现bug

--- src/layout/components/SideBar/Item.vue
+++ src/layout/components/SideBar/Item.vue
@@ -1,21 +1,23 @@
-<template>
-    <div>
-        <svg-icon :icon-class="icon" />
-        <span slot="title">{{title}}</span>
-    </div>
-</template>
 <script>
 export default {
-	name: "MenuItem",
-	props: {
-		icon: {
-			type: String,
-			default: ""
-		},
-		title: {
-			type: String,
-			default: ""
+    name: 'MenuItem',
+    functional: true,
+    props: {
+        icon: {
+            type: String,
+            default: ''
+        },
+        title: {
+            type: String,
+            default: ''
         }
+    },
+    render(h, context) {
+        const { icon, title } = context.props
+        const vnode = []
+        vnode.push(<svg-icon icon-class={icon} />)
+        vnode.push(<span slot="title">{title}</span>)
+        return vnode
     }
-};
+}
 </script>
