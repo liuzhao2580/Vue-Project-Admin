76db61abbbbba8966988e6c2a29e397810f6701b
liuzhao2580 <1457639270@qq.com>
2020-10-21 15:51:15 +0800

    编写简单的webpack-plugin

---
+++ plugin/copyRight-webpack-plugin.js
@@ -0,0 +1,21 @@
+class CopyRight {
+    constructor() {}
+    apply(compiler) {
+        compiler.hooks.emit.tapAsync('CopyRight', (compliation, cb) => {
+            compliation.assets['copyRight.txt'] = {
+                source: () => {
+                    return 'copyright by q'
+                },
+                size: () => {
+                    return 15
+                }
+            }
+            cb()
+        })
+        compiler.hooks.compile.tap('CopyRight', (compliation) => {
+            console.log(compliation, 1111)
+        })
+    }
+}
+
+module.exports = CopyRight
