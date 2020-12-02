220f8c5b75e8b37e9748db0e07ae72e8d6c9019d
liuzhao2580 <1457639270@qq.com>
2020-12-02 10:28:29 +0800

    格式化部分代码

--- src/views/components/markdown/index.vue
+++ src/views/components/markdown/index.vue
@@ -1,36 +1,41 @@
 <template>
-	<div class="markdown-box">
-        <p> 依赖于 mavon-editor <a href="https://github.com/hinesboy/mavonEditor" style="color: #409eff" target="block">GitHub地址</a> </p>
-        <mavon-editor @save="mavon_save" v-model="mavon_model" :ishljs = "true"/>
+    <div class="markdown-box">
+        <p>
+            依赖于 mavon-editor
+            <a href="https://github.com/hinesboy/mavonEditor" style="color: #409eff" target="block"
+                >GitHub地址</a
+            >
+        </p>
+        <mavon-editor @save="mavon_save" v-model="mavon_model" :ishljs="true" />
         <el-button type="primary" @click="onSubmit" class="tinymce-btn">提交</el-button>
     </div>
 </template>
 
 <script>
-import {mavonEditor} from 'mavon-editor'
+import { mavonEditor } from 'mavon-editor'
 import 'mavon-editor/dist/css/index.css'
 export default {
-	name: "markdown",
-	components: {
+    name: 'markdown',
+    components: {
         mavonEditor
     },
-	props: {},
-	data() {
-		return {
-            mavon_model: ""
-        };
-	},
-	mounted() {},
-	methods: {
-        mavon_save(value ,render) {
-            console.log(value,render)
+    props: {},
+    data() {
+        return {
+            mavon_model: ''
+        }
+    },
+    mounted() {},
+    methods: {
+        mavon_save(value, render) {
+            console.log(value, render)
         },
         onSubmit() {
             console.log(this.mavon_model)
         }
     },
-	watch: {}
-};
+    watch: {}
+}
 </script>
 
 <style lang="scss" scoped>
