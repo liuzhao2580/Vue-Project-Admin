9d1212eb3c3823cafe6b63c893daf4ba9ccc6591
liuzhao2580 <1457639270@qq.com>
2020-12-03 17:01:48 +0800

    将 路由动画 和面包屑动画的css样式写在组件内部

--- src/layout/components/Breadcrumb/index.vue
+++ src/layout/components/Breadcrumb/index.vue
@@ -1,20 +1,28 @@
 <template>
     <div class="breadcrumb-box">
         <el-breadcrumb separator-class="el-icon-arrow-right">
-            <el-breadcrumb-item :to="{ path: '/' }">{{breadcrumbTitle}}</el-breadcrumb-item>
+            <el-breadcrumb-item :to="{ path: '/' }">{{ breadcrumbTitle }}</el-breadcrumb-item>
             <!--面包屑的切换动画 -->
-            <transition-group name="breadcrumb-transition" mode="out-in" v-if="getMatched.length !== 0">
-                <el-breadcrumb-item  v-for="item in getMatched" :key="item.path" :to="handleClick(item)">{{item.meta.title}}</el-breadcrumb-item>
+            <transition-group
+                name="breadcrumb-transition"
+                mode="out-in"
+                v-if="getMatched.length !== 0"
+            >
+                <el-breadcrumb-item
+                    v-for="item in getMatched"
+                    :key="item.path"
+                    :to="handleClick(item)"
+                    >{{ item.meta.title }}</el-breadcrumb-item
+                >
             </transition-group>
         </el-breadcrumb>
     </div>
 </template>
 
 <script>
-/* eslint-disable */
-import defaultConfig from "@/setting"
+import defaultConfig from '@/setting'
 export default {
-    name: "breadcrumb",
+    name: 'breadcrumb',
     computed: {
         breadcrumbTitle() {
             return defaultConfig.title
@@ -22,18 +30,18 @@ export default {
     },
     data() {
         return {
-            getMatched : []
+            getMatched: []
         }
     },
     created() {},
     methods: {
         init() {
             const nowRoute = this.$route.matched
-            this.getMatched = nowRoute.filter(item => {
+            this.getMatched = nowRoute.filter((item) => {
                 // 说明存在多级路由
                 if (item.meta && item.meta.title) {
                     // 用于判断 当前页面是否在首页
-                    if (item.path == "/dashboard") return false
+                    if (item.path == '/dashboard') return false
                     // 用于判断当前的路由是否显示在面包屑中
                     else if (item.meta.breadcrumb == false) return false
                     return true
@@ -43,7 +51,7 @@ export default {
         // 面包屑点击
         handleClick(item) {
             // 用于判断当前的面包屑是否可以点击跳转
-            if (item.redirect !== "noRedirect") {
+            if (item.redirect !== 'noRedirect') {
                 return item.redirect
             }
         }
@@ -56,8 +64,26 @@ export default {
             immediate: true
         }
     }
-};
+}
 </script>
 
-<style>
+<style lang="scss" scoped>
+/*---------------------------------面包屑的过度动画----------------------- */
+.breadcrumb-transition-enter,
+.breadcrumb-transition-leave {
+    opacity: 0.2;
+}
+.breadcrumb-transition-enter {
+    opacity: 0;
+    transform: translateX(50px);
+}
+.breadcrumb-transition-leave {
+    opacity: 0;
+    transform: translateX(-50px);
+}
+.breadcrumb-transition-enter-active,
+.breadcrumb-transition-leave-active {
+    transition: 0.7s;
+}
+/*---------------------------------面包屑的过度动画----------------------- */
 </style>
