0ef73ee0676311f948e4f30e50c1cc9a61cdecc7
liuzhao2580 <1457639270@qq.com>
2021-01-13 17:18:59 +0800

    重新请求返回的数据格式

--- src/api/modules/user.ts
+++ src/api/modules/user.ts
@@ -1,12 +1,13 @@
 import axios from '../axiosConfig'
+import { ResultModel } from '@/typescript/model'
 
 // 用户登录
-export const userLogin = (params: any) => {
+export const userLogin = (params: any): Promise<ResultModel<any>> => {
     return axios.post(`/login`, params)
 }
 
 // 获取指定的用户信息
-export const userInfoApi = (params: any) => {
+export const userInfoApi = (params: any): Promise<ResultModel<any>> => {
     return axios.get(`/userInfo`, { params })
 }
 
