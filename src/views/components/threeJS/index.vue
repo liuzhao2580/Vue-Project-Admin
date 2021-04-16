e7d9c306d7975e7afd523d16aa42169acdeb29fa
liuzhao2580 <1457639270@qq.com>
2021-04-16 18:06:33 +0800

    添加threejs部分字段

--- src/views/components/threeJS/index.vue
+++ src/views/components/threeJS/index.vue
@@ -6,15 +6,27 @@
         >文档地址</a
       >
     </p>
+    <div id="threeJS"></div>
   </div>
 </template>
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator"
+import THREE from "three"
 @Component({
   name: "threeJSComponents",
 })
 export default class ThreeJSComponents extends Vue {
   name12 = "1234"
+  mounted() {}
+  /** 初始化 */
+  init() {
+    // 1. 创建一个摄像头
+    const sceen = new THREE.Scene()
+    // 2. 创建一个相机
+    const camera = new THREE.Camera()
+    // 3. 创建一个渲染器
+    const renderer = new THREE.WebGLRenderer()
+  }
 }
 </script>
 
