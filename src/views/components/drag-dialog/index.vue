4c835af3ff3c76aed2cb118b14e74a1534536069
小火车况且况且 <1457639270@qq.com>
2022-04-28 18:30:16 +0800

    :art: 可拖拽的 dialog 使用element-plus的属性

--- src/views/components/drag-dialog/index.vue
+++ src/views/components/drag-dialog/index.vue
@@ -4,15 +4,12 @@
       >点击打开 Dialog</el-button
     >
     <el-dialog
-      v-el-drag-dialog
-      v-model:visible="dialogVisible"
+      v-model="dialogVisible"
       width="30%"
       :close-on-click-modal="false"
-      @closed="dialog_closed"
-      class="dialog-box"
-      ref="dialogREF"
+      draggable
     >
-      <!-- <span slot="title">这是一段信息</span> -->
+      <template #title>这是一段信息</template>
       <template #footer>
         <div class="dialog-footer">
           <el-button @click="dialogVisible = false">取 消</el-button>
@@ -26,19 +23,9 @@
 </template>
 
 <script lang="ts" setup>
-// import dragDialog from '@/directives/el-drag-dialog'
 import { ref } from 'vue'
 
 const dialogVisible = ref<boolean>(false)
-
-const dialogREF = ref<HTMLDivElement>()
-
-const dialog_closed = () => {
-  if (dialogREF.value) {
-    dialogREF.value.style.left = '0'
-    dialogREF.value.style.top = '0'
-  }
-}
 
 </script>
 
@@ -50,9 +37,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.dialog-box {
-  :deep(.el-dialog) {
-    margin-bottom: 0;
-  }
-}
 </style>
