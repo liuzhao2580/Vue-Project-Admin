63277eb0245420ea3e1f70bf0c22cd635d8b00a7
小火车况且况且 <1457639270@qq.com>
2022-06-10 19:36:02 +0800

    :fire: 尝试添加 G6 的拓扑图

---
+++ src/views/components/topology/G6utils.ts
@@ -0,0 +1,276 @@
+/** 实例 */
+let graph: any = null
+
+const G6 = window.G6
+
+enum CustomTypeEnum {
+  /** 圆形 */
+  circleType = 'circle-type',
+  /** 菱形 */
+  diamondType = 'diamond-type'
+}
+
+interface INodesLabel {
+  id: string
+  type: CustomTypeEnum
+  panels: {
+    title: string
+    label: string
+  }
+}
+
+const nodesLabel: INodesLabel[] = [
+  {
+    id: '1',
+    type: CustomTypeEnum.circleType,
+    panels: {
+      title: '发起',
+      label: '小火车况且况且'
+    }
+  },
+  {
+    id: '2',
+    type: CustomTypeEnum.diamondType,
+    panels: {
+      title: '费用承担人',
+      label: '小火车'
+    }
+  },
+  {
+    id: '3',
+    type: CustomTypeEnum.diamondType,
+    panels: {
+      title: '一级主管审核',
+      label: '小汽车'
+    }
+  },
+  {
+    id: '4',
+    type: CustomTypeEnum.diamondType,
+    panels: {
+      title: '二级主管审核',
+      label: '小飞机'
+    }
+  },
+  {
+    id: '5',
+    type: CustomTypeEnum.diamondType,
+    panels: {
+      title: '三级主管审核',
+      label: '小轮船'
+    }
+  },
+  {
+    id: '6',
+    type: CustomTypeEnum.diamondType,
+    panels: {
+      title: '法务审批',
+      label: '小火箭'
+    }
+  },
+  {
+    id: '7',
+    type: CustomTypeEnum.diamondType,
+    panels: {
+      title: '会计审核',
+      label: '小卡车'
+    }
+  },
+  {
+    id: '8',
+    type: CustomTypeEnum.diamondType,
+    panels: {
+      title: '财务经理',
+      label: '小拖车'
+    }
+  },
+  {
+    id: '9',
+    type: CustomTypeEnum.diamondType,
+    panels: {
+      title: '财务总监',
+      label: '小火车'
+    }
+  },
+  {
+    id: '10',
+    type: CustomTypeEnum.diamondType,
+    panels: {
+      title: 'CFO',
+      label: 'CFO'
+    }
+  },
+  {
+    id: '11',
+    type: CustomTypeEnum.diamondType,
+    panels: {
+      title: '提交纸质单据',
+      label: '财务扫描枪'
+    }
+  },
+  {
+    id: '12',
+    type: CustomTypeEnum.diamondType,
+    panels: {
+      title: '待付款',
+      label: '总账审批'
+    }
+  },
+  {
+    id: '13',
+    type: CustomTypeEnum.diamondType,
+    panels: {
+      title: '付款',
+      label: '财务付款-自动'
+    }
+  },
+  {
+    id: '14',
+    type: CustomTypeEnum.diamondType,
+    panels: {
+      title: '出纳核销',
+      label: '出纳核销归档'
+    }
+  },
+  {
+    id: '15',
+    type: CustomTypeEnum.circleType,
+    panels: {
+      title: '归档',
+      label: '归档'
+    }
+  }
+]
+function loopNodes(loopList: INodesLabel[]): any {
+  let loopX = 1
+  /** 一行有多少个数据 */
+  const lineNumber = 6
+  return loopList.map((itemLabel, index) => {
+    const nodesMap = {
+      label: itemLabel.panels.label,
+      x: 0,
+      y: 0,
+      ...itemLabel
+    }
+    if (index % lineNumber > 0) {
+      loopX++
+    } else {
+      loopX = 1
+    }
+    const getFloor = Math.floor(index / lineNumber)
+
+    // 说明是偶数,顺序是从左往右
+    if (getFloor % 2 === 0) {
+      nodesMap.x = 150 * loopX
+    }
+    // 说明是奇数, 顺序是从右往左
+    else {
+      nodesMap.x = 150 * (lineNumber - loopX + 1)
+    }
+    nodesMap.y = 80 * (getFloor + 1)
+    return nodesMap
+  })
+}
+const data = {
+  // 点集
+  nodes: loopNodes(nodesLabel),
+  edges: [
+    {
+      type: 'polyline',
+      source: '1', // 起始点 id
+      target: '2', // 结束点 id
+      style: {
+        stroke: 'pink'
+      }
+    },
+    {
+      source: '1', // 起始点 id
+      target: '3', // 结束点 id
+      type: 'line-arrow',
+      style: {}
+    }
+  ]
+}
+// 自定义边界线条
+G6.registerEdge(
+  'line-arrow',
+  {
+    draw(cfg: any, group: any) {
+      console.log(cfg)
+      const startPoint = cfg.startPoint
+      const endPoint = cfg.endPoint
+      return group.addShape('path', {
+        attrs: {
+          stroke: 'red',
+          path: [
+            ['M', startPoint.x, startPoint.y],
+            ['L', startPoint.x, endPoint.y / 2],
+            ['L', endPoint.x, endPoint.y / 2],
+            ['L', endPoint.x, endPoint.y]
+          ]
+        },
+        name: 'path-shape'
+      })
+    }
+  },
+  'line'
+)
+
+// 自定义 node 元素 圆形
+G6.registerNode(CustomTypeEnum.circleType, {
+  drawShape
+})
+
+function drawShape(cfg: any, group: any) {
+  console.log(cfg, 'cfg')
+  // const { panels } = cfg
+  const shape = group.addShape('circle', {
+    attrs: {
+      x: 0,
+      y: 0,
+      r: 20,
+      fill: 'blue',
+      cursor: 'pointer'
+    }
+  })
+  return shape
+}
+export default {
+  init: () => {
+    const container = document.getElementById('container') as HTMLDivElement
+    const width = container.scrollWidth
+    const height = container.scrollHeight || 500
+    graph = new G6.Graph({
+      container: 'container',
+      width,
+      height,
+      fitCenter: true,
+      defaultEdge: {
+        style: {
+          lineAppendWidth: 3,
+          endArrow: true
+        }
+      }
+    })
+
+    graph.data(data)
+    graph.render()
+
+    graph.on('node:mouseenter', (e: any) => {
+      graph.setItemState(e.item, 'active', true)
+    })
+    graph.on('node:mouseleave', (e: any) => {
+      graph.setItemState(e.item, 'active', false)
+    })
+    graph.on('edge:mouseenter', (e: any) => {
+      graph.setItemState(e.item, 'active', true)
+    })
+    graph.on('edge:mouseleave', (e: any) => {
+      graph.setItemState(e.item, 'active', false)
+    })
+  },
+  /** 销毁 */
+  graphDestory() {
+    graph.destroy()
+  }
+}
--- src/views/components/topology/index.vue
+++ src/views/components/topology/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="topology-box">
-    <p>
+    <p class="topology-box-title">
       <a
         href="https://antv-g6.gitee.io/zh/docs/manual/getting-started"
         target="_block"
@@ -12,114 +12,15 @@
 </template>
 
 <script lang="ts" setup>
-import { onMounted } from 'vue'
+import { onMounted, onUnmounted } from 'vue'
+import G6Utils from './G6utils'
 onMounted(() => {
-  init()
+  G6Utils.init()
 })
 
-const init = () => {
-  const data = {
-    nodes: [
-      {
-        id: '0',
-        label: 'node-0',
-        x: 100,
-        y: 100,
-        description: 'This is node-0.',
-        subdescription: 'This is subdescription of node-0.'
-      },
-      {
-        id: '1',
-        label: 'node-1',
-        x: 250,
-        y: 100,
-        description: 'This is node-1.',
-        subdescription: 'This is subdescription of node-1.'
-      },
-      {
-        id: '2',
-        label: 'node-2',
-        x: 150,
-        y: 310,
-        description: 'This is node-2.',
-        subdescription: 'This is subdescription of node-2.'
-      },
-      {
-        id: '3',
-        label: 'node-3',
-        x: 320,
-        y: 310,
-        description: 'This is node-3.',
-        subdescription: 'This is subdescription of node-3.'
-      }
-    ],
-    edges: [
-      {
-        id: 'e0',
-        source: '0',
-        target: '1',
-        description: 'This is edge from node 0 to node 1.'
-      },
-      {
-        id: 'e1',
-        source: '0',
-        target: '2',
-        description: 'This is edge from node 0 to node 2.'
-      },
-      {
-        id: 'e2',
-        source: '0',
-        target: '3',
-        description: 'This is edge from node 0 to node 3.'
-      }
-    ]
-  }
-  const tooltip = new window.G6.Tooltip({
-    offsetX: 10,
-    trigger: 'click',
-    offsetY: 0,
-    // v4.2.1 起支持 fixToNode，tooltip 相对于节点固定位置
-    fixToNode: [1, 0],
-    // the types of items that allow the tooltip show up
-    // 允许出现 tooltip 的 item 类型
-    itemTypes: ['node'],
-    // custom the tooltip's content
-    // 自定义 tooltip 内容
-    getContent: (e: any | undefined) => {
-      const outDiv = document.createElement('div')
-      outDiv.style.width = 'fit-content'
-      outDiv.innerHTML = `
-          <h4>Custom Content</h4>
-          <ul>
-            <li>Type: ${e.item.getType()}</li>
-          </ul>
-          <ul>
-            <li>Label: ${e.item.getModel().label || e.item.getModel().id}</li>
-          </ul>`
-      return outDiv
-    }
-  })
-  const container = document.getElementById('container') as HTMLDivElement
-  const width = container.scrollWidth
-  const height = container.scrollHeight || 500
-  const graph = new window.G6.Graph({
-    container: 'container',
-    width,
-    height,
-    linkCenter: true,
-    plugins: [tooltip],
-    modes: {
-      default: ['drag-node']
-    },
-    defaultNode: {
-      size: [80, 40],
-      type: 'rect'
-    }
-  })
-  graph.data(data)
-  graph.render()
-}
-this
+onUnmounted(()=> {
+  G6Utils.graphDestory()
+})
 </script>
 
 <script lang="ts">
@@ -131,6 +32,9 @@ export default {
 
 <style lang="scss" scoped>
 .topology-box {
+  &-title {
+    height: 40px;
+  }
   #container {
     height: 600px;
   }
