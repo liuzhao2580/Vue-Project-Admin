e2f7bc3e6e4fb79c63137014da95af7726453255
小火车况且况且 <1457639270@qq.com>
2022-06-22 17:41:09 +0800

    :art: 优化 types.d.ts 的写法

--- src/process.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'process' {
-  global {
-    namespace NodeJS {
-      interface ProcessEnv extends Dict<string> {
-        NODE_ENV?: 'development' | 'production' | 'pages' | 'test'
-      }
-    }
-  }
-}
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,7 +1,14 @@
-export {}
-declare global {
-  interface Window {
-    DDLogin(object: { id; goto; style; width; height }): void
-    G6: any
+interface Window {
+  DDLogin(object: { id; goto; style; width; height }): void
+  G6: any
+}
+declare module 'process' {
+  global {
+    namespace NodeJS {
+      interface ProcessEnv extends Dict<string> {
+        [key: string] : string | undefined
+        NODE_ENV?: 'development' | 'production' | 'pages' | 'test'
+      }
+    }
   }
 }
