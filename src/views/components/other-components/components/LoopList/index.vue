aedf771218d8892b8f56c9af62ec1191bc2fa1f4
小火车况且况且 <liuzhao2580@163.com>
2023-03-21 18:25:11 +0800

    :art: 添加滑动功能

--- src/views/components/other-components/components/LoopList/index.vue
+++ src/views/components/other-components/components/LoopList/index.vue
@@ -1,9 +1,80 @@
 <template>
-  这是一个测试
+  <div class="loop-list-box">
+    <ul class="list-box">
+      <li class="item" v-for="index in data.listLength" :key="index">
+        {{ "小火车的车厢" + index }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { onMounted, shallowReactive, onUnmounted } from "vue";
 
+interface IData {
+  loopListBox: HTMLDivElement | null;
+  listBox: HTMLDivElement | null;
+  item: HTMLDivElement | null;
+  intervalId: undefined | NodeJS.Timer;
+  listLength: number;
+}
+
+const data = shallowReactive<IData>({
+  loopListBox: null,
+  listBox: null,
+  item: null,
+  intervalId: undefined,
+  listLength: 50
+});
+
+let getListBoxHight = 0;
+let getLoopListBoxHeight = 0;
+let timeoutId: undefined | NodeJS.Timer = undefined;
+onMounted(() => {
+  setTimeout(() => {
+    init();
+    getListBoxHight = data.listBox?.scrollHeight as number;
+    getLoopListBoxHeight = data.loopListBox?.offsetHeight as number;
+  }, 1000);
+});
+
+const init = () => {
+  data.loopListBox = document.querySelector(".loop-list-box");
+  data.listBox = document.querySelector(".list-box");
+  data.item = document.querySelector(".item");
+  data.intervalId = setInterval(intervalEvent, 20);
+};
+
+function intervalEvent() {
+  if (data.loopListBox && data.listBox) {
+    if (data.loopListBox.scrollTop + getLoopListBoxHeight >= getListBoxHight) {
+      data.loopListBox?.addEventListener("scroll", loopListScrollEvent);
+      clearInterval(data.intervalId);
+      data.intervalId = undefined;
+    } else {
+      data.loopListBox.scrollTop++;
+    }
+  }
+}
+function loopListScrollEvent() {
+  if(data.intervalId) {
+    clearTimeout(timeoutId);
+    return;
+  }
+  if (timeoutId) {
+    clearTimeout(timeoutId);
+    timeoutId = undefined;
+  }
+  timeoutId = setTimeout(() => {
+    timeoutId = undefined;
+    data.intervalId = setInterval(intervalEvent, 20);
+  }, 500);
+}
+onUnmounted(() => {
+  clearInterval(data.intervalId);
+  data.intervalId = undefined;
+  data.loopListBox?.removeEventListener("scroll", loopListScrollEvent);
+});
 </script>
 
 <script lang="ts">
@@ -13,6 +84,17 @@ export default {
 };
 </script>
 
-
 <style scoped lang="scss">
+.loop-list-box {
+  height: 400px;
+  overflow: auto;
+  transition: all 1s;
+  .list-box {
+    padding: 0 20px;
+    .item {
+      text-align: center;
+      padding-bottom: 10px;
+    }
+  }
+}
 </style>
