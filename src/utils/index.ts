f4367228f70b68a330af875345ce728a462fe94b
小火车况且况且 <liuzhao2580@163.com>
2023-03-24 11:00:48 +0800

    :ambulance: 修复一个封装的图片方法, 处理打包图片不显示

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -73,8 +73,9 @@ function validKeyFlag(
 /** vite的特殊性, 需要处理图片 */
 export const handleViteImages = (imgPath: string) => {
   try {
-    const handlePath = imgPath.replace("@", "..");
-    return new URL(handlePath, import.meta.url).href;
+    const getSplit = imgPath.split("/");
+    const getName = getSplit[getSplit.length - 1];
+    return new URL(`../assets/images/${getName}`, import.meta.url).href;
   } catch (error) {
     console.warn(error);
   }
