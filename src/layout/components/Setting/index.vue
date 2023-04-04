37415984287e199f2647788d6be301b580f78b83
小火车况且况且 <liuzhao2580@163.com>
2023-04-04 18:42:26 +0800

    :art: 尝试添加切换颜色功能

--- src/layout/components/Setting/index.vue
+++ src/layout/components/Setting/index.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="setting-com-box">
-    <div class="setting-icon">
+    <div class="setting-icon" @click="setClick">
       <span class="iconfont icon-setting"></span>
     </div>
     <el-drawer v-model="visible">
       <div>Hi, there!</div>
+      <ThemeColorSelect></ThemeColorSelect>
     </el-drawer>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue"
+import ThemeColorSelect from "./components/ThemeColorSelect/index.vue"
 
 const visible = ref(false)
+
+const setClick = ()=> {
+  visible.value = true
+}
 </script>
 
 <script lang="ts">
@@ -28,6 +34,11 @@ export default {
     position: fixed;
     right: 0;
     top: 50%;
+    background-color: pink;
+    padding: 16px;
+    border-top-left-radius: 6px;
+    border-bottom-left-radius: 6px;
+    cursor: pointer;
   }
 }
 </style>
