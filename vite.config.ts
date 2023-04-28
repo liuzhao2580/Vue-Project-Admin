cc2ae8a8fea1e5449467f4327318251709d5ff20
小火车况且况且 <liuzhao2580@163.com>
2023-04-28 14:33:41 +0800

    :bug: 处理部署到GitHub-pages的打包, 解决没有前缀的问题

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,9 @@ const isBuildPlugins = (command: "build" | "serve") => {
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd())
+  const base = mode === "pages" ? "/Vue-Project-Admin/" : "/"
   return {
+    base,
     server: {
       host: "0.0.0.0",
       port: 9527,
